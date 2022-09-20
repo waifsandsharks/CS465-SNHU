2111dbc354d7eb34ea44012799ace8f388858125
waifsandsharks <Waifsandsharks@Gmail.com>
2022-09-20 17:55:00 -0500

    converted static html to json for travel

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,9 @@
+const fs = required('fs');
+const trips = JSON.parse (fs.readfileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-res.render('travel',  { title: 'Travlr Getaways'});
+res.render('travel',  { title: pageTitle, trips});
 };
 
 module.exports = {
