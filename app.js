d5d41139d2b936147aea6eabf87bd2956cb0e886
waifsandsharks <Waifsandsharks@Gmail.com>
2022-09-20 15:41:08 -0500

    created a route and controller for travel

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const hbs = require('hbs');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
+
 
 var app = express();
 
@@ -25,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app_server/routes/travel.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const controller = require('../controllers/travel');
+
+
+/* GET home page. */
+router.get('/', controller.travel);
+
+module.exports = router;
