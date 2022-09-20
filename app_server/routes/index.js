36925efb44b012deba73dc3b889d7523f0228e73
waifsandsharks <Waifsandsharks@Gmail.com>
2022-09-20 14:01:54 -0500

    Rebuilt main

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const controller = require('../controllers/main');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
