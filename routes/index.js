71402f91bb234198a3f659ea89896867768d5d62
waifsandsharks <Waifsandsharks@Gmail.com>
2022-09-20 16:07:34 -0500

    updated gitignore to include vscode

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const controller = require('../controllers/main');
-
 
 /* GET home page. */
-router.get('/', controller.index);
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
-
