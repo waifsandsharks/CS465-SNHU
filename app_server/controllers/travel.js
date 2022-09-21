1223aa463041280f2cec3b95b4b76ca7fbe78a06
waifsandsharks <Waifsandsharks@Gmail.com>
2022-09-20 20:37:38 -0500

    converted more static to JSON - Bug control fixed bad syntax.

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,10 @@
-const fs = required('fs');
-const trips = JSON.parse (fs.readfileSync('./data/trips.json', 'utf8'));
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
 const travel = (req, res) => {
-res.render('travel',  { title: pageTitle, trips});
+    pageTitle = process.env.npm_package_description + ' - Travel';
+    res.render('travel',  {title: pageTitle, trips});
 };
 
 module.exports = {
