3fb99a2f458f7e92a4272b2d83056fbb8d3da5e8
waifsandsharks <Waifsandsharks@Gmail.com>
2022-10-09 22:26:52 -0500

    -Set up SPA --installed angular using: npm install @angular/cli@v6-lts --Generated new angular project ng new travlr-admin --defaults=true --directory app_admin -- installed angular csi -- generated links for bootstrap -- imported test data -- generated a trip listing component ng generate component edit-trip

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
 
 app.set('view engine', 'hbs');
 
@@ -26,6 +26,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Allow CORS
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
